01e1dfc270025845c6a4731a151718cb086f98ac
Ramazan_Zyamilov <nash96@bk.ru>
2022-08-02 22:01:05 +0400

    created  ex054

---
+++ ex05/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Здравсвтуйте, введите Ваше имя:");
+string username = Console.ReadLine();
+if (username.ToLower() == "влада")
+{
+   Console.WriteLine("Привет, любовь моя!"); 
+}
+else 
+{
+    Console.Write("Привет,");
+    Console.WriteLine(username);
+
+}
