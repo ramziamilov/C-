8ca5781cc48a805116fc09134c6e5f3029617cfe
Ramazan_Zyamilov <nash96@bk.ru>
2022-08-08 20:01:57 +0400

    программа для поиска индекса массива по числу

---
+++ lesson2_massivi_i_funkcii/ex03_poisk_elemnta_massiva/Program.cs
@@ -0,0 +1,18 @@
+﻿// поиск индекса массива по значению
+int[] array = { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+int n = array.Length;
+
+int find = 4;
+
+int index = 0;
+
+while (index < n)
+{
+
+    if (array[index] == find)
+    {
+        System.Console.WriteLine(index);
+    }
+    index++;
+}
