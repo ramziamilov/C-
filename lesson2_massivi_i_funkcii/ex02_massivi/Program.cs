6659fcdd5b0b6a674ee99506621baf0d27535191
Ramazan_Zyamilov <nash96@bk.ru>
2022-08-08 19:51:41 +0400

    сделаи программу для нахождения макс из 9 с массивом

---
+++ lesson2_massivi_i_funkcii/ex02_massivi/Program.cs
@@ -0,0 +1,22 @@
+﻿// нахождение максимума из 9 чисел путем массива
+int Max(int arg1, int arg2, int arg3) // записали функцию
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+int[] array = { 1, 23, 31, 489, 5, 65, 757, 88, 49 };
+
+
+
+int max= Max (
+    Max(array[0], array[1], array[2]), // вызов функции
+  Max(array[3], array[4], array[5]),
+   Max(array[6], array[7], array[8])
+   );
+
+
+System.Console.WriteLine(max);
+
