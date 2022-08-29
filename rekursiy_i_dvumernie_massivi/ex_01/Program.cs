63034a5054231e3fcf31e41a47013c4157ee5d79
Ramazan_Zyamilov <nash96@bk.ru>
2022-08-29 18:21:44 +0400

    двумерные массивы

---
+++ rekursiy_i_dvumernie_massivi/ex_01/Program.cs
@@ -0,0 +1,41 @@
+﻿// Двумерные массивы
+/*
+string[,] table = new string[2, 3];
+
+for (int rows = 0; rows < 2; rows++) // количесвто строк 2, поэтому <2!
+{
+    for (int columns = 0; columns < 5; columns++) // количество столбцов
+    {
+System.Console.WriteLine($"-{table[rows, columns]}-");
+    }
+}
+*/
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++) // количесвто строк 2, поэтому <2!
+    {
+        for (int j = 0; j < matr.GetLength(1); j++) // количество столбцов
+        {
+            System.Console.Write($"{matr[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(0, 100); // [0; 99]
+        }
+    }
+}
+
+
+int[,] matrix = new int[3, 4];
+PrintArray(matrix);
+Console.WriteLine();
+FillArray(matrix);
+PrintArray(matrix);
